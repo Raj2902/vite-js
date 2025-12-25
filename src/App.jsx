0b9692df6ba5336667b045791aee960d44d748f0
Raj Agarwal <rajagarwalgood@gmail.com>
2025-12-25 07:08:12 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { TEST } from "./enviroments/enviroment";
+
+function App() {
+  console.log(TEST);
+
+  return (
+    <>
+      <h1>Hi there</h1>
+    </>
+  );
+}
+
+export default App;
